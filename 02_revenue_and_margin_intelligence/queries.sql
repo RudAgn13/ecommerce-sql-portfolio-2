cf501fe3810f8cd3c41775a134fcffeb465c33d6
RudAgn13 <roodagnihotri@gmail.com>
2026-06-06 03:34:57 +0530

    Update queries.sql

--- 02_revenue_and_margin_intelligence/queries.sql
+++ 02_revenue_and_margin_intelligence/queries.sql
@@ -17,6 +17,25 @@ group by date_trunc('month', o.order_date)
 order by order_month;
 
 --METRIC 2: Contribution Margin by Category
+select
+	ca.parent_category,
+    ca.category_name,
+    count(distinct oi.order_id) total_delivered_orders,
+    sum(oi.line_total) gross_category_revenue,
+    sum(oi.quantity*p.cost_price) total_cogs,
+    sum(case when oi.is_returned=true then oi.line_total else 0 end) return_value,
+    round(sum(oi.line_total*ca.commission_rate),2) platform_commission,
+    --platforms have to pay commission for all order status, because txn takes place
+    sum(oi.line_total)-sum(oi.quantity*p.cost_price)-sum(case when oi.is_returned=true then oi.line_total else 0 end) gross_profit,
+    round(100.0*(sum(oi.line_total)-sum(oi.quantity*p.cost_price)-sum(case when oi.is_returned=true then oi.line_total else 0 end))/nullif(sum(oi.line_total),0),2) gross_profit_pct,
+    round(sum(oi.line_total*ca.commission_rate)-sum(case when oi.is_returned=true then oi.line_total else 0 end),2) net_platform_take
+from categories ca
+join products p on ca.category_id = p.category_id
+join order_items oi on p.product_id = oi.product_id
+join orders o on oi.order_id = o.order_id
+where o.order_status = 'delivered'
+group by ca.parent_category, ca.category_name
+order by gross_profit desc;
 
 --METRIC 3: Discount Depth Analysis
 
