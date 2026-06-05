7d6ecd7b6d3a8446dbd50def92ab5f6ef8137b41
RudAgn13 <roodagnihotri@gmail.com>
2026-06-05 21:02:15 +0530

    Update queries.sql

--- 01_customer_lifecycle_analysis/queries.sql
+++ 01_customer_lifecycle_analysis/queries.sql
@@ -13,3 +13,17 @@ from customers c
 join orders o on c.customer_id = o.customer_id
 where o.order_status = 'delivered'
 group by c.customer_id;
+
+-- METRIC 2: AVERAGE ORDER VALUE (AOV) BY CUSTOMER TIER
+select
+	c.tier,
+    count(distinct c.customer_id) customers_in_tier,
+    count(distinct o.order_id) total_delivered_orders,
+    sum(o.net_amount) total_revenue,
+    round(sum(o.net_amount)/nullif(count(distinct o.order_id),0),2) AOV,
+    round(1.0*count(distinct o.order_id)/nullif(count(distinct c.customer_id),0),2) avg_orders_per_customer
+from customers c
+join orders o on c.customer_id = o.customer_id
+where o.order_status = 'delivered'
+group by c.tier
+order by aov desc;
